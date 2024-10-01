13b882507e8ad03185bc2d2a1c892408791abb9c
raised-by-bees <167509203+raised-by-bees@users.noreply.github.com>
2024-10-01 13:38:43 +0100

    Update cisco_ise_failed_attempts_handler.py

--- cisco_ise_failed_attempts_handler.py
+++ cisco_ise_failed_attempts_handler.py
@@ -1,51 +1,26 @@
-import psycopg2
-import psycopg2.extras
-from psycopg2 import sql
 import logging
 from datetime import datetime
 import re
-
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/ciscoise"
+from database_utils import fta_inserter, fwa_inserter, fla_inserter
 
 def handle_cisco_ise_failed_attempts(ip, message):
     log_data = parse_syslog_message(message)
 
     # Determine the table name based on conditions
     if 'Failed-Attempt: Authentication failed' in message and 'Protocol=Tacacs' in message:
-        table_name = 'fta'
-        fields = ('timestamp', 'ipaddress', 'username', 'nasipaddress', 'remoteaddress', 'failurereason', 'networkdevicename', 'requestlatency')
         row_data = [datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ip, log_data.get('UserName'), log_data.get('Device IP Address'),
                     log_data.get('RemoteAddress'), log_data.get('FailureReason'), log_data.get('NetworkDeviceName'), log_data.get('RequestLatency')]
+        fta_inserter.add_to_batch(tuple(row_data))
     elif 'WLC' in log_data.get('NetworkDeviceName', '') and 'HO' in log_data.get('CalledStationID', ''):
-        table_name = 'fwa'
-        fields = ('timestamp', 'ipaddress', 'username', 'nasipaddress', 'calledstationid', 'failurereason', 'networkdevicename')
         row_data = [datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ip, log_data.get('UserName'), log_data.get('NASIPAddress'),
                     log_data.get('CalledStationID'), log_data.get('FailureReason'), log_data.get('NetworkDeviceName')]
+        fwa_inserter.add_to_batch(tuple(row_data))
     elif '-' in log_data.get('NetworkDeviceName', ''):
-        table_name = 'fla'
-        fields = ('timestamp', 'ipaddress', 'username', 'nasipaddress', 'nasportid', 'failurereason', 'networkdevicename')
         row_data = [datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ip, log_data.get('UserName'), log_data.get('NASIPAddress'),
                     log_data.get('NasPortID'), log_data.get('FailureReason'), log_data.get('NetworkDeviceName')]
+        fla_inserter.add_to_batch(tuple(row_data))
     else:
         return  # Exit if neither condition is met
-
-    conn = psycopg2.connect(DATABASE_URL)
-    cursor = conn.cursor()
-
-    # Create the SQL insert statement dynamically
-    insert_stmt = sql.SQL('INSERT INTO {} ({}) VALUES %s').format(
-        sql.Identifier(table_name),
-        sql.SQL(', ').join(map(sql.Identifier, fields))
-    )
-
-    try:
-        psycopg2.extras.execute_values(cursor, insert_stmt, [tuple(row_data)])
-        conn.commit()
-    except Exception as error:
-        logging.error(f"Error inserting data into PostgreSQL: {error}")
-    finally:
-        cursor.close()
-        conn.close()
 
 def parse_syslog_message(message):
     field_patterns = {
